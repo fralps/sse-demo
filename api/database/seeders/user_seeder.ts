93402c377e8f7ce46f2404b8b4e80521ecdfc1cd
fralps <francois@kinoba.fr>
2025-06-17 09:57:23 +0200

    chore: remove deprecated API documentation and related user model files

--- api/database/seeders/user_seeder.ts
+++
@@ -1,8 +0,0 @@
-import { UserFactory } from '#database/factories/user_factory'
-import { BaseSeeder } from '@adonisjs/lucid/seeders'
-
-export default class extends BaseSeeder {
-  async run() {
-    await UserFactory.createMany(30)
-  }
-}
