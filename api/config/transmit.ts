4a225e125ecb08459d8ad73f334f3470328c9eb2
fralps <francois@kinoba.fr>
2025-06-17 11:01:33 +0200

    style: add comments to clarify configuration options in transmit.ts

--- api/config/transmit.ts
+++ api/config/transmit.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@adonisjs/transmit'
 
 export default defineConfig({
-  pingInterval: 5000,
-  transport: null
+  pingInterval: 5000, // keep the connection alive
+  transport: null // use Redis to sync the events across multiple instances
 })
